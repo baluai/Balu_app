0302699bf5fbe537e642bc766d61893bbaf36b91
baluai <144058253+baluai@users.noreply.github.com>
2023-09-04 16:31:26 -0300

    Baluaiapp.py

--- Baluaiapp.py
+++ Baluaiapp.py
@@ -12,6 +12,8 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+# Cargar el modelo de transcripción de whisper
+model = whisper.load_model("base")
 
 # Ruta de la carpeta de textos
 text_folder = "C:\\Users\\cleme\\Desktop\\Brave up\\Brave up transcripciones\\"
