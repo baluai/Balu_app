9b8caa3b8ed739b2c7aed44bea9061e59c2662de
baluai <144058253+baluai@users.noreply.github.com>
2023-10-16 12:10:16 -0300

    Update Baluaiapp.py

--- Baluaiapp.py
+++ Baluaiapp.py
@@ -1,96 +1,69 @@
-import openai
-import streamlit as st
-import whisper
 import os
-import time
-import tempfile
+import whisper
+import pyodbc
 
-st.title("BALUAI")
-
-st.title("Verificación de Instalación de Whisper")
-
-try:
-    st.write(f"Módulo 'whisper' importado correctamente.")
-    st.write(f"Directorio del módulo: {whisper.__file__}")
-except Exception as e:
-    st.error("Error al verificar la instalación de Whisper.")
-    st.write(str(e))
-
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+# Configuración de la conexión a la base de datos
+conn_str = (
+    "Driver={SQL Server};"
+    "Server=sql5110.site4now.net;"
+    "Database=db_a76939_bbddproyecto;"
+    "UID=db_a76939_bbddproyecto_admin;"
+    "PWD=9!vXkv2212222;"
+    "Trusted_Connection=no;"
+)
 
 # Cargar el modelo de transcripción de whisper
 model = whisper.load_model("small")
 
-# Subir archivo de audio
-uploaded_file = st.file_uploader("Sube un archivo de audio (mp3 o wav)", type=["mp3", "wav"])
+# Consulta SQL para insertar nuevos registros en la tabla RegistroLlamadas
+insert_query = "INSERT INTO RegistroLlamadas (Fecha, Transcripcion,NombreVendedor,NombreEmpresa, Proceso,NombreArchivo,IdEmpresa,IdVendedor) values (GETDATE(),?,?,?,?,?,3,18)"
 
-# Inicializar la transcripción
-transcription_text = ""
+# Ruta de la carpeta de archivos de audio
+audio_folder = "C:\\Users\\cleme\\Desktop\\Audios Balu\\ICGroup\\100 grabaciones MCE para análisis_septiembre 2023\\"
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+try:
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    # Recorrer todas las subcarpetas de audio_folder
+    for root, _, _ in os.walk(audio_folder):
 
-st.chat_message("assistant").write("Sube tu archivos aqui")
+    # Obtén la lista de archivos de audio en la carpeta actual
+        audio_files = [f for f in os.listdir(root) if f.lower().endswith(('.mp3', '.gsm', '.wav'))]
+    
+        for audio_file_name in audio_files:
+            audio_file_path = os.path.join(root, audio_file_name)
 
-if uploaded_file is not None:
+            # Realiza la transcripción
+            result = model.transcribe(audio_file_path, language="es")
+            transcription_text = result["text"]
 
-    # Crea un archivo temporal
-    temp_audio_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_audio_path = temp_audio_file.name
-    temp_audio_file.write(uploaded_file.read())
-    temp_audio_file.close()
+            #print(audio_file_path.encode('utf-8', 'ignore').decode('utf-8'))
 
-    with st.spinner("Transcribiendo... ⏳ _Demora entre 20 y 40% de la diración del audio_"):
+            # Dividir la ruta del archivo en variables
+       
+            split_path = audio_file_path.split(os.path.sep)
+            NombreArchivo = split_path[-1]  # Nombre de la empresa
+            SubCarpeta1 = split_path[-2]  # Nombre del proceso
+            SubCarpeta2 = split_path[-3]  # Nombre del vendedor
+            SubCarpeta3 = split_path[-4]   # Nombre de la carpeta 1
 
-        _, probs = model.detect_language(mel)
-        
-        # Realiza la transcripción
-        result = model.transcribe(temp_audio_path) #, language="es")
+         #C:\Users\cleme\Desktop\Audios Balu\ICGroup\100 grabaciones MCE para análisis_septiembre 2023\No Ventas
 
-        # Obtén el texto de la transcripción
-        transcription_text = result["text"]
+            # Dividir la fecha del nombre del archivo (suponiendo que la fecha está en el nombre)
+            filename_parts = os.path.splitext(audio_file_name)[0].split('_')
+            if len(filename_parts) >= 2:
+               NVendedor = filename_parts[-1]
+            else:
+                NVendedor = 'No hay Nvendedor en el nombre'
 
-    # Mostrar mensaje de transcripción exitosa en el chat
-    st.chat_message("assistant").write("Transcripción exitosa! ✅")
+             
 
-# Mostrar las primeras 25 palabras de la transcripción en el chat
-if transcription_text:
-    words = transcription_text.split()
-    display_text = ' '.join(words[:50]) + "..." if len(words) > 50 else transcription_text
-    st.chat_message("assistant").write(f"Transcripción: {display_text}")
 
-    # React to user input # Display user message in chat message container
-if prompt := st.chat_input("Haz una pregunta"):
-    additional_info = " [Esta es una transcripcion que no es perfecta, tu objetivo es rescatar insights valiosos y informacion relevante]"
-    prompt_with_info = prompt + additional_info
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append({"role": "assistant", "content": transcription_text})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    # Crea un espacio vacío para la respuesta del asistente
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            max_tokens=1000,
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+            # Actualizar la base de datos con la respuesta generada
+            cursor.execute(insert_query, transcription_text, NVendedor, SubCarpeta3,SubCarpeta2,NombreArchivo) 
+            conn.commit()
+            
+except pyodbc.Error as ex:
+    conn.rollback()  # En caso de error, se revierte la transacción
+    print("Error de conexion o consulta:", ex)
